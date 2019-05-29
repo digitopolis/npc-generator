98400607b8cef1ee58dd50c3499185bfd80d0326
Matt Readout <matt.readout@gmail.com>
2019-05-29 18:04:01 -0400

    fetch from 5e api

---
+++ src/apiEndpoints.js
@@ -0,0 +1,2 @@
+export const RACES = 'http://www.dnd5eapi.co/api/races'
+export const CLASSES = 'http://www.dnd5eapi.co/api/classes'
---
+++ src/containers/Generator.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Options from '../components/Options'
+import Result from '../components/Result'
+import { RACES, CLASSES } from '../apiEndpoints'
+
+class Generator extends React.Component {
+
+	state = {
+		races: [],
+		classes: [],
+		result: null
+	}
+
+	async componentDidMount() {
+		const racesResponse = await fetch(RACES).then(res => res.json())
+		const classesResponse = await fetch(CLASSES).then(res => res.json())
+
+		const races = racesResponse.results
+		const classes = classesResponse.results
+
+		this.setState({ races, classes })
+	}
+
+	generateResult = () => {
+		const result = {}
+		result.race = this.state.races[(Math.floor(Math.random() * this.state.races.length))].name
+		result.class = this.state.classes[(Math.floor(Math.random() * this.state.classes.length))].name
+		this.setState({ result })
+	}
+
+	render () {
+		return (
+			<div className='container'>
+				<Options getResult={this.generateResult}/>
+				<Result {...this.state.result}/>
+			</div>
+		)
+	}
+}
+
+export default Generator;

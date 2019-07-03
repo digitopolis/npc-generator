aaf108308417a7b471905100de9f0ce60f4d86dc
Matt Readout <matt.readout@gmail.com>
2019-07-03 19:57:16 -0400

    re-wire with data obj

--- src/containers/Generator.jsx
+++ src/containers/Generator.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import Options from '../components/Options'
 import Result from '../components/Result'
-import { RACES, CLASSES } from '../apiEndpoints'
+// import { RACES, CLASSES } from '../apiEndpoints'
+import { DATA } from '../data'
 
 class Generator extends React.Component {
 
 	state = {
-		races: [],
-		classes: [],
+		races: DATA.races,
+		classes: DATA.classes,
 		result: null
 	}
 
-	async componentDidMount() {
-		const racesResponse = await fetch(RACES).then(res => res.json())
-		const classesResponse = await fetch(CLASSES).then(res => res.json())
-
-		const races = racesResponse.results
-		const classes = classesResponse.results
-
-		this.setState({ races, classes })
-	}
+	// async componentDidMount() {
+	// 	const racesResponse = await fetch(RACES).then(res => res.json())
+	// 	const classesResponse = await fetch(CLASSES).then(res => res.json())
+	//
+	// 	const races = racesResponse.results
+	// 	const classes = classesResponse.results
+	//
+	// 	this.setState({ races, classes })
+	// }
 
 	generateResult = () => {
 		const result = {}
-		result.race = this.state.races[(Math.floor(Math.random() * this.state.races.length))].name
+		const randomRace = this.state.races[(Math.floor(Math.random() * this.state.races.length))]
+		console.log(randomRace);
+		result.race = randomRace.name
+		result.subrace = randomRace.subraces[(Math.floor(Math.random() * randomRace.subraces.length))]
 		result.class = this.state.classes[(Math.floor(Math.random() * this.state.classes.length))].name
 		result.lvl = Math.floor(Math.random() * 20) + 1
 		this.setState({ result })
